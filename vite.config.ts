dfa62eb990449c6123fd7a7c7353c9f37f66522e
Lukas Lieb <lukas.lieb@axonivy.com>
2025-04-02 16:19:49 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ export default defineConfig({
   plugins: [],
   build: { outDir: 'build', chunkSizeWarningLimit: 5000, rollupOptions: { input: { index: './index.html', mock: './mock.html' } } },
   server: {
-    port: 3000
+    port: 3010,
+    proxy: {
+      '/designer': {
+        target: 'http://localhost:8081/',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   },
   base: './'
 });
